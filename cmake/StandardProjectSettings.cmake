475fd08de4cd5896bf4bf38f87ca1ba3a5ba5a75
j.spijker@ultimaker.com <jelle spijker>
2022-04-14 11:51:01 +0200

    Renamed assure_out_of_source_builds
    codestyle
    
    Contributes to CURA-8640

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -241,7 +241,7 @@ function(set_project_warnings project_name)
 endfunction()
 
 # This function will prevent in-source builds
-function(AssureOutOfSourceBuilds)
+function(assure_out_of_source_builds)
     # make sure the user doesn't play dirty with symlinks
     get_filename_component(srcdir "${CMAKE_SOURCE_DIR}" REALPATH)
     get_filename_component(bindir "${CMAKE_BINARY_DIR}" REALPATH)
@@ -258,7 +258,7 @@ endfunction()
 
 option(ALLOW_IN_SOURCE_BUILD "Allow building in your source folder. Strongly discouraged" OFF)
 if(NOT ALLOW_IN_SOURCE_BUILD)
-    assureoutofsourcebuilds()
+    assure_out_of_source_builds()
 endif()
 
 function(enable_sanitizers project_name)
