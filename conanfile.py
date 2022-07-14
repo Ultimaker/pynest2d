c68518463c777ca4fc3790b94a667facf9499433
j.spijker@ultimaker.com <jelle spijker>
2022-07-14 18:12:32 +0200

    Use umbase 0.1.4
    Contributes to CURA-9365

--- conanfile.py
+++ conanfile.py
@@ -20,7 +20,7 @@ class PyNest2DConan(ConanFile):
     revision_mode = "scm"
     exports = "LICENSE*"
 
-    python_requires = "umbase/0.1.1@ultimaker/testing", "sipbuildtool/0.1@ultimaker/testing"
+    python_requires = "umbase/0.1.4@ultimaker/testing", "sipbuildtool/0.1@ultimaker/testing"
     python_requires_extend = "umbase.UMBaseConanfile"
 
     options = {
@@ -39,7 +39,7 @@ class PyNest2DConan(ConanFile):
     }
 
     def requirements(self):
-        for req in self._um_data(self.version)["requirements"]:
+        for req in self._um_data(self.version, self.channel)["requirements"]:
             self.requires(req)
 
     def config_options(self):
