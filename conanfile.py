d3a5ff876d67d8903bbdf006d86094d7c26ced16
j.spijker@ultimaker.com <jelle spijker>
2022-08-22 11:56:24 +0200

    Use CMake build modules from  sipbuildtool and umbase
    COntributes to CURA-8788

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 from conan.tools import files
-from conan.tools.layout import cmake_layout
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan import ConanFile
 from conans import tools
 
@@ -21,7 +21,7 @@ class PyNest2DConan(ConanFile):
     revision_mode = "scm"
     exports = "LICENSE*"
 
-    python_requires = "umbase/0.1.5@ultimaker/testing", "pyprojecttoolchain/0.1.3@ultimaker/testing", "sipbuildtool/0.2.1@ultimaker/testing"
+    python_requires = "umbase/0.1.6@ultimaker/stable", "pyprojecttoolchain/0.1.5@ultimaker/stable", "sipbuildtool/0.2.2@ultimaker/stable"
     python_requires_extend = "umbase.UMBaseConanfile"
 
     options = {
@@ -44,6 +44,8 @@ class PyNest2DConan(ConanFile):
     }
 
     def requirements(self):
+        self.requires("umbase/0.1.6@ultimaker/stable")  # required for the CMake build modules
+        self.requires("sipbuildtool/0.2.2@ultimaker/stable")  # required for the CMake build modules
         for req in self._um_data()["requirements"]:
             self.requires(req)
 
@@ -60,10 +62,33 @@ class PyNest2DConan(ConanFile):
             tools.check_min_cppstd(self, 17)
 
     def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+
         pp = self.python_requires["pyprojecttoolchain"].module.PyProjectToolchain(self)
         pp.blocks["tool_sip_project"].values["sip_files_dir"] = Path("python").as_posix()
         pp.blocks.remove("extra_sources")
         pp.generate()
+
+        tc = CMakeToolchain(self)
+        tc.variables["Python_EXECUTABLE"] = self.deps_user_info["cpython"].python.replace("\\", "/")
+        tc.variables["Python_USE_STATIC_LIBS"] = not self.options["cpython"].shared
+        tc.variables["Python_ROOT_DIR"] = self.deps_cpp_info["cpython"].rootpath.replace("\\", "/")
+        tc.variables["Python_FIND_FRAMEWORK"] = "NEVER"
+        tc.variables["Python_FIND_REGISTRY"] = "NEVER"
+        tc.variables["Python_FIND_IMPLEMENTATIONS"] = "CPython"
+        tc.variables["Python_FIND_STRATEGY"] = "LOCATION"
+        tc.variables["Python_SITEARCH"] = "site-packages"
+
+        if self.settings.compiler == "Visual Studio":
+            tc.blocks["generic_system"].values["generator_platform"] = None
+            tc.blocks["generic_system"].values["toolset"] = None
+        tc.generate()
+
+        # Generate the Source code from SIP
+        sip = self.python_requires["sipbuildtool"].module.SipBuildTool(self)
+        sip.configure()
+        sip.build()
 
     def layout(self):
         cmake_layout(self)
@@ -72,19 +97,19 @@ class PyNest2DConan(ConanFile):
             self.cpp.package.system_libs = ["pthread"]
 
     def build(self):
-        sip = self.python_requires["sipbuildtool"].module.SipBuildTool(self)
-        sip.configure()
-        sip.build()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def package(self):
         packager = files.AutoPackager(self)
-        packager.patterns.build.lib = ["*.so", "*.so.*", "*.a", "*.lib", "*.dylib", "*.pyd", "*.pyi"]
+        packager.patterns.build.lib = ["*.so", "*.so.*", "*.a", "*.lib", "*.dylib", "*.pyd"]
         packager.run()
 
-        files.files.rmdir(self, os.path.join(self.package_folder, "lib", "pynest2d"))
+        self.copy("*.pyi", src = os.path.join(self.build_folder, "pynest2d"), dst = os.path.join(self.package_folder, "lib"), keep_path = False)
 
     def package_info(self):
         if self.in_local_cache:
-            self.runenv_info.append_path("PYTHONPATH", os.path.join(self.package_folder, "site-packages"))
+            self.runenv_info.append_path("PYTHONPATH", os.path.join(self.package_folder, "lib"))
         else:
-            self.runenv_info.append_path("PYTHONPATH", os.path.join(self.build_folder, "site-packages"))
+            self.runenv_info.append_path("PYTHONPATH", self.build_folder)
