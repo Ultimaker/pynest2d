448bfa1774c1da9d8842fa8786e586436c0689f6
Remco Burema <r.burema@ultimaker.com>
2023-09-08 22:25:40 +0200

    Require main branch of libnest2d dependency.
    after-merge care for CURA-10951

--- conanfile.py
+++ conanfile.py
@@ -66,7 +66,7 @@ class PyNest2DConan(ConanFile):
         copy(self, "*", path.join(self.recipe_folder, "python"), path.join(self.export_sources_folder, "python"))
 
     def requirements(self):
-        self.requires("nest2d/(latest)@ultimaker/cura_10951")
+        self.requires("nest2d/(latest)@ultimaker/testing")
         self.requires("cpython/3.10.4")
 
     def validate(self):
