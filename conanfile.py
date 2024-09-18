93f75a9af27b53fc7f649c4fa970771d0aff75ce
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2024-09-18 10:14:46 +0200

    Add global dependency to clipper and nlopt
    This should not be required, but it is for some reason. It seems that linking against libnest2d is somehow omitted, and pynest2d then misses required level 2 dependencies

--- conanfile.py
+++ conanfile.py
@@ -71,6 +71,8 @@ class PyNest2DConan(ConanFile):
         for req in self.conan_data["requirements"]:
             self.requires(req)
         self.requires("cpython/3.12.2")
+        self.requires("clipper/6.4.2")
+        self.requires("nlopt/2.7.1")
 
     def validate(self):
         if self.settings.compiler.cppstd:
